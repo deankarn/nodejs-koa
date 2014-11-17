d667b73dc7d08a2f94ede657d20b6ed37be6394e
Dean Karn <Dean.Karn@metricaid.com>
2014-11-17 14:13:22 -0500

    db config change

--- app/config/database.js
+++ app/config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 module.exports = {
 
-		'url' : 'mongodb://anthony:root@127.0.0.1:27017/node_js'
-        // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+		'url' : 'mongodb://mongodbtestuser:kJUMWqmMFs9Hk9z3bAYUtwR@127.0.0.1:27017/nodejs_templatev1'
+		// looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
